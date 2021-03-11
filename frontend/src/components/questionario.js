721f47f96078835670e070529fbc00b7262ce350
Mateus <Mateus Moreira>
2021-03-11 00:32:35 -0300

    Implementacao forms
    Co-authored-by: Pedro Lima <phlimas@outlook.com>
    Co-authored-by: Philipe Serafim <philipeserafim@gmail.com>
    
    Signed-off-by: Mateus <Mateus Moreira>

--- frontend/src/components/questionario.js
+++ frontend/src/components/questionario.js
@@ -5,7 +5,7 @@ import womanFigura from "../images/woman.png";
 const Questionario = () => {
   return (
     <section id="questionario">
-      <div id='titulo'>
+      <div id="titulo">
         <h1>Questionário</h1>
       </div>
       <div id="container">
@@ -14,20 +14,20 @@ const Questionario = () => {
             Sofreu violência física? Seja de modo que ofenda sua integridade ou
             saúde corporal.
           </p>
-          <p>
-            Sofreu violência física? Seja de modo que ofenda sua integridade ou
-            saúde corporal.
-          </p>
-          <p>
-            Sofreu violência física? Seja de modo que ofenda sua integridade ou
-            saúde corporal.
-          </p>
+          <form>
+            <label class = 'label-formulario' for="">Questão 1:</label><br/>
+            <input type="text" id="fname" class = 'input-formulario' name="fname"/><br/>
+            <label class = 'label-formulario' for="">Questão 2:</label><br/>
+            <input type="text" id="fname" class = 'input-formulario' name="fname"/><br/>
+            <label class = 'label-formulario' for="">Questão 3:</label><br/>
+            <input type="text" id="fname" class = 'input-formulario' name="fname"/><br/>
+          </form>
         </div>
         <div id="foto">
           <img id="img-formulario" src={womanFigura} />
         </div>
       </div>
-      <div id='btn-formulario'>
+      <div id="btn-formulario">
         <button id="btn-enviar">Enviar</button>
       </div>
     </section>
