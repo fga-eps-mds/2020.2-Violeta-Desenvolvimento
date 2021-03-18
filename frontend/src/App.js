5502b85f0a00607c852545a801a956058def0e22
victorhdcoelho <victorhdcoelho@gmail.com>
2021-03-18 10:52:29 -0300

    Revert "Formulario para Questionario Frontend"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/header';
-import Home from './components/home';
-import Questionario from './components/questionario';
-import Footer from './components/footer';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <Home />
-                <Questionario />
-                <Footer />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
 export default App;
