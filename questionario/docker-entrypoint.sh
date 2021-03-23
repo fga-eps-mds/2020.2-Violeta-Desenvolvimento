289c11128b930a190d0220d5219ebd743b581103
Victor Hugo Dias Coelho <victorhdcoelho@gmail.com>
2021-03-23 13:14:35 -0300

    Arrumando heroku

--- questionario/docker-entrypoint.sh
+++ questionario/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/user/bin/env ash
 
-POSTGRES_PORT="5432"
-FRONTEND_PORT="3000"
 
 wait_ready() {
 echo "Aguardando o $1 na porta $2"
@@ -10,6 +8,5 @@ until nc -z $1 $2 ; do
 done
 }
 
-wait_ready db $POSTGRES_PORT
-wait_ready frontend $FRONTEND_PORT
+wait_ready db $DB_HOST $DB_PORT
 exec python3 manage.py runserver 0.0.0.0:8001
