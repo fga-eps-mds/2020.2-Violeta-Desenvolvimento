4b1ba1a795efff4dda0aad1280317098f5ec4af6
Iago <iago.andradedf@gmail.com>
2021-03-23 22:42:58 -0300

    Correção de segurança do código

--- questionario/questionario/views.py
+++ questionario/questionario/views.py
@@ -1,8 +1,10 @@
 import json
 from django.http import HttpResponse
 from .models import Questionario
+from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(["GET"])
 def analisa_fluxos(request):
     # Iniciando as variáveis
     arquivo = open('questionario/entrada.json')
@@ -61,7 +63,6 @@ def analisa_fluxos(request):
                                         resultado.append(
                                             str(quest.categoria_violencia))
                                         # Psicológica
-
     # Questão 2
     for quest in quests:
         # Alguém próximo a você se feriu de alguma forma?
@@ -86,7 +87,6 @@ def analisa_fluxos(request):
                         if resp_db['label'] == resp_in['label'] and resp_in['label'] == 'Sim':
                             resultado.append(str(quest.categoria_violencia))
                             # Psicológica
-
     # Questão 3
     for quest in quests:
         # Já foi acusada indevidamente, por traição ou mentiras?
@@ -96,14 +96,16 @@ def analisa_fluxos(request):
             resp_in = perg_in['children'][0]
             resp_db = perg_db['children'][0]
             if resp_db['label'] == resp_in['label'] and resp_in['label'] == 'Sim':
-                # Isso lhe causou algum constrangimento como xingamentos, humilhações e/ou exposições?
+                # Isso lhe causou algum constrangimento como xingamentos,
+                # humilhações e/ou exposições?
                 perg_in = resp_in
                 perg_db = resp_db
                 if perg_db['question'] == perg_in['question']:
                     resp_in = perg_in['children'][0]
                     resp_db = perg_db['children'][0]
                     if resp_db['label'] == resp_in['label'] and resp_in['label'] == 'Sim':
-                        # Esse acontecimento foi usado como justificativa para algum tipo de agressão física?
+                        # Esse acontecimento foi usado como justificativa para
+                        # algum tipo de agressão física?
                         perg_in = resp_in
                         perg_db = resp_db
                         if perg_db['question'] == perg_in['question']:
@@ -117,7 +119,7 @@ def analisa_fluxos(request):
                             elif resp_db['label'] == resp_in['label'] and resp_in['label'] == 'Não':
                                 resultado.append(
                                     str(quest.categoria_violencia))
-
     # Mostrando resultado
+    resultado = list(set(resultado))
     html = "<html><body>%s</body></html>" % resultado
     return HttpResponse(html)
