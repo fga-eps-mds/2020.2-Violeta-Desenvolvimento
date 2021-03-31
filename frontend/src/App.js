9eee02172594434b0da9860e11d17e1ec202cafd
PedroLimass <phlimas@outlook.com>
2021-03-31 19:52:34 -0300

    Subindo dependencias e alterações pendentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    //   Link
+} from 'react-router-dom';
+
 import './App.css';
-import Header from './components/header';
-import Home from './components/home';
-import Questionario from './components/questionario';
-import Depoimento from './components/depoimento';
-import Footer from './components/footer';
+// import Header from './components/header';
+// import Home from './components/home';
+// import Questionario from './components/questionario';
+// import Depoimento from './components/depoimento';
+// import Footer from './components/footer';
+import AllComponentes from './components/AllComponentes';
+import Login from './components/login';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Header />
-                <Home />
-                <Questionario />
-                <Depoimento />
-                <Footer />
-            </div>
+            <Router>
+                <Switch>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/">
+                        <AllComponentes />
+                    </Route>
+                </Switch>
+            </Router>
         );
     }
 }
