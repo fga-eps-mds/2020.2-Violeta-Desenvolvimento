c62aea2d259db84d9853602d309b2e1089e4b632
Iago <iago.andradedf@gmail.com>
2021-03-31 23:54:18 -0300

    Adicionada página de administrador
    Co-authored-by: Pedro Lima <phlimas@outlook.com>

--- frontend/src/components/adm.js
+++ frontend/src/components/adm.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import '../css/adm.css';
 
 const Adm = () => (
-    <div>
-        <h1>Adm</h1>
+    <div id="adm-container">
+        <div class="adm-cadastro">
+            <h1>Cadastro de Profissional</h1>
+            <input placeholder="Nome" class="input-adm" />
+            <input placeholder="Contato" class="input-adm" />
+            <input placeholder="Área de atuação" class="input-adm" />
+            <textarea placeholder="Descrição" class="input-adm" />
+            <button type="button" class="btn-login">
+                Cadastrar
+            </button>
+        </div>
+        <div class="adm-depoimento">
+            <h1>Aprovação de Depoimento</h1>
+        </div>
     </div>
 );
 
