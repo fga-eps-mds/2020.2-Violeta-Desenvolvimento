699e67feceb3ce03d31d74804a9b056827b0b45a
Iago <iago.andradedf@gmail.com>
2021-03-31 21:54:01 -0300

    Adicionada tela de login
    Co-authored-by: Pedro Lima <pedrolimas@outlook.com>

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import '../css/login.css';
 
 const Login = () => (
-    <div>
-        <h1>OLawRRR</h1>
+    <div id="login-container">
+        <div class="login">
+            <h1>Login</h1>
+            <input placeholder="username" class="input" />
+            <input placeholder="senha" class="input" />
+            <button type="button" class="btn-login">
+                Entrar
+            </button>
+        </div>
     </div>
 );
 
