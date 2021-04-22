7359712c6d7119db19e9d33fd1bff6b8e225a456
Philipe Serafim <philipeserafim@gmail.com>
2021-04-21 23:31:36 -0300

    Corrigido detalhes do css
    Signed-off-by: Philipe Serafim <philipeserafim@gmail.com>

--- frontend/src/components/adm.js
+++ frontend/src/components/adm.js
@@ -23,22 +23,24 @@ const Adm2 = ({ logout }) => {
     }
 
     return (
-        <div id="adm-container" class="adm-container">
-            <div class="adm-cadastro">
-                <h1>Cadastro de Profissional</h1>
-                {<CadastraProfissional />}
-            </div>
-            <div class="adm-depoimento">
-                <h1>Aprovação de Depoimento</h1>
-                <div id="container-depoimento">
-                    <div class="container-p-depoimento">
-                        {<ListaDepoimento />}
+        <div>
+            <div id="adm-container" class="adm-container">
+                <div class="adm-cadastro">
+                    <h1>Cadastro de Profissional</h1>
+                    {<CadastraProfissional />}
+                </div>
+                <div class="adm-depoimento">
+                    <h1>Aprovação de Depoimento</h1>
+                    <div id="container-depoimento">
+                        <div class="container-p-depoimento">
+                            {<ListaDepoimento />}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div id="adm-frase">
-                <h1>Cadastro de Frases</h1>
-                {<CadastrarFrases />}
+                <div id="adm-frase">
+                    <h1>Cadastro de Frases</h1>
+                    {<CadastrarFrases />}
+                </div>
             </div>
             <button class="btn-logout" onClick={(e) => handleClick(e)}>
                 Logout
