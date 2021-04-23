cbd1563ac35d33a4ea3b8000ea47e8a0639a9089
Philipe Serafim <philipeserafim@gmail.com>
2021-04-22 22:12:48 -0300

    Adicionado html e css para frases estáticas
    Co-authored-by: Wesley Santos <wesley.sluk@hotmail.com>

--- frontend/src/components/AllComponentes.js
+++ frontend/src/components/AllComponentes.js
@@ -6,14 +6,18 @@ import Depoimento from './depoimento';
 import Footer from './footer';
 import Profissionais from './profissionais';
 import Graficos from './graficos';
+import Frases from './visualizarFrases';
 
 const AllComponentes = () => (
     <section>
         <Header />
         <Home />
         <Questionario />
+        <Frases />
         <Depoimento />
+        <Frases />
         <Graficos />
+        <Frases />
         <Profissionais />
         <Footer />
     </section>
---
+++ frontend/src/components/visualizarFrases.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../css/visualizarFrases.css';
+
+class FrasesMotivacionais extends Component {
+    render() {
+        return (
+            <div class="frases">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec egestas mollis ipsum quis auctor. Nullam mattis lacus
+                    sit amet massa consectetur, at ornare elit imperdiet. Proin
+                    nulla nisi, molestie sit amet purus a, hendrerit dictum leo.
+                    Curabitur blandit finibus est eget pellentesque.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default FrasesMotivacionais;
