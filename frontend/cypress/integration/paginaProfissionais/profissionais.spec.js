1df54b88037633bc3a01f7cf7f29f460ff555cd5
Mateus Moreira <mateuuusss@gmail.com>
2021-04-28 22:49:59 -0300

    Adicionado teste de nav-bar e corrigido parte profissionais

--- frontend/cypress/integration/paginaProfissionais/profissionais.spec.js
+++ frontend/cypress/integration/paginaProfissionais/profissionais.spec.js
@@ -3,4 +3,19 @@ describe('Page Profissionais', () => {
         cy.visit('/#profissionais');
         cy.expect('Profissionais').to.exist;
     });
+
+    it('Should return true if exists description in Profissionais page', () => {
+        cy.visit('/#profissionais');
+        cy.expect('#profissionais-description').to.exist;
+    });
+
+    it('Get response when click on nav-bar', () => {
+        cy.visit('/#profissionais');
+        cy.contains('Ongs').click();
+    });
+
+    it('Verify if pagination-mobile is display none', () => {
+        cy.visit('/#profissionais');
+        cy.get('.pagination-mobile').should('not.be.visible');
+    });
 });
