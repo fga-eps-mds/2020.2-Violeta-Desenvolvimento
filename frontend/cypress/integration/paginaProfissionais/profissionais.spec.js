2f7d63f902e5401a6a741aa0a14cc618960d85a4
Mateus Moreira <mateuuusss@gmail.com>
2021-04-30 20:47:31 -0300

    Adicionado testes página profissionais

--- frontend/cypress/integration/paginaProfissionais/profissionais.spec.js
+++ frontend/cypress/integration/paginaProfissionais/profissionais.spec.js
@@ -1,21 +1,27 @@
 describe('Page Profissionais', () => {
+    const profissionaisPage = '/#profissionais';
     it('Should return true if exists title Profissionais', () => {
-        cy.visit('/#profissionais');
+        cy.visit(profissionaisPage);
         cy.expect('Profissionais').to.exist;
     });
 
     it('Should return true if exists description in Profissionais page', () => {
-        cy.visit('/#profissionais');
+        cy.visit(profissionaisPage);
         cy.expect('#profissionais-description').to.exist;
     });
 
+    it('Return true if has box-contact displayed', () => {
+        cy.visit(profissionaisPage);
+        cy.get('#container-profissionais').should('exist');
+    });
+
     it('Get response when click on nav-bar', () => {
-        cy.visit('/#profissionais');
+        cy.visit(profissionaisPage);
         cy.contains('Ongs').click();
     });
 
     it('Verify if pagination-mobile is display none', () => {
-        cy.visit('/#profissionais');
+        cy.visit(profissionaisPage);
         cy.get('.pagination-mobile').should('not.be.visible');
     });
 });
