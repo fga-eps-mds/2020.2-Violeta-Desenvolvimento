e709abbf1d4111f06c58506fb002f0d75b909c81
Iago <iago.andradedf@gmail.com>
2021-04-30 21:46:52 -0300

    Adicionado swagger do questionario

--- questionario/questionario/viewsets.py
+++ questionario/questionario/viewsets.py
@@ -1,33 +1,84 @@
 from rest_framework import viewsets
 from . import models
-from . import serializers
+from .serializers import (
+    ViolenciasCountSerializer,
+    CategoriaViolenciaSerializer,
+    CategoriaContatoSerializer,
+    ContatoViolenciaSerializer,
+    QuestionarioSerializer,
+    ContatoQuestionarioSerializer
+)
+
+from drf_yasg.utils import swagger_auto_schema
 
 
 class ViolenciasCountViewset(viewsets.ModelViewSet):
     queryset = models.ViolenciasCount.objects.all()
-    serializer_class = serializers.ViolenciasCountSerializer
+    serializer_class = ViolenciasCountSerializer
+
+    @swagger_auto_schema(
+        request_body=ViolenciasCountSerializer,
+        response={200: ViolenciasCountSerializer},
+        operation_description="Lista de ocorrências de cada tipo de violência")
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class CategoriaViolenciaViewset(viewsets.ModelViewSet):
     queryset = models.CategoriaViolencia.objects.all()
-    serializer_class = serializers.CategoriaViolenciaSerializer
+    serializer_class = CategoriaViolenciaSerializer
+
+    @swagger_auto_schema(
+        request_body=CategoriaViolenciaSerializer,
+        response={200: CategoriaViolenciaSerializer},
+        operation_description="Lista de categorias de violência")
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class CategoriaContatoViewset(viewsets.ModelViewSet):
     queryset = models.CategoriaContato.objects.all()
-    serializer_class = serializers.CategoriaContatoSerializer
+    serializer_class = CategoriaContatoSerializer
+
+    @swagger_auto_schema(
+        request_body=CategoriaContatoSerializer,
+        response={200: CategoriaContatoSerializer},
+        operation_description="Lista de categorias dos contatos")
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class ContatoViolenciaViewset(viewsets.ModelViewSet):
     queryset = models.ContatoViolencia.objects.all()
-    serializer_class = serializers.ContatoViolenciaSerializer
+    serializer_class = ContatoViolenciaSerializer
+
+    @swagger_auto_schema(
+        request_body=ContatoViolenciaSerializer,
+        response={200: ContatoViolenciaSerializer},
+        operation_description="Lista de contatos de profissionais")
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class QuestionarioViewset(viewsets.ModelViewSet):
     queryset = models.Questionario.objects.all()
-    serializer_class = serializers.QuestionarioSerializer
+    serializer_class = QuestionarioSerializer
+
+    @swagger_auto_schema(
+        request_body=QuestionarioSerializer,
+        response={200: QuestionarioSerializer},
+        operation_description="Armazenamento da árvore de decisões do questionário")
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class ContatoQuestionarioViewset(viewsets.ModelViewSet):
     queryset = models.ContatoQuestionario.objects.all()
-    serializer_class = serializers.ContatoQuestionarioSerializer
+    serializer_class = ContatoQuestionarioSerializer
+
+    @swagger_auto_schema(
+        request_body=ContatoQuestionarioSerializer,
+        response={200: ContatoQuestionarioSerializer},
+        operation_description="Lista de contatos indicados ao responder o questionário")
+    def perform_create(self, serializer):
+        serializer.save()
