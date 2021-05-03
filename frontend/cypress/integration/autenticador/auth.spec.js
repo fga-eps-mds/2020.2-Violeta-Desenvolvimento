c80b819cdc3b7d9e741eb5627387483796bf5749
Mateus Moreira <mateuuusss@gmail.com>
2021-05-03 20:16:47 -0300

    Feito testes de autenticador
    Co-authored-by: Wesley Santos <wesley.sluk@hotmail.com>

---
+++ frontend/cypress/integration/autenticador/auth.spec.js
@@ -0,0 +1,61 @@
+describe('Autenticador Page', () => {
+    it('verifies if the login autentication page is valid', () => {
+        Cypress.env('userLogin', 'testandoTeste');
+        const userTest = [
+            {
+                username: Cypress.env('userLogin'),
+                password: Cypress.env('userLogin'),
+            },
+        ];
+        cy.visit('/login/');
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8004/autenticador/autenticador/login/',
+            body: {
+                username: userTest[0].username,
+                password: userTest[0].password,
+            },
+            failOnStatusCode: false,
+        }).then((response) => {
+            expect(response.body.username).is.not.null;
+            cy.log(response.body.username);
+            Cypress.env('loginStatus', response.status);
+
+            if (response.status === 401) {
+                cy.request({
+                    method: 'POST',
+                    url: 'http://localhost:8004/autenticador/account/register',
+                    body: {
+                        username: userTest[0].username,
+                        password: userTest[0].password,
+                    },
+                }).then((response) => {
+                    expect(response.body).has.property('username');
+                    expect(response.body.username).is.not.null;
+                    cy.log(response.body.username);
+                    Cypress.env('createUsarioID', response.body.username);
+                });
+                cy.visit('/login/');
+                cy.get('[type="text"]').type('testandoTeste');
+                cy.get('[type="password"]').type('testandoTeste');
+                cy.get('.btn-login').click();
+            } else {
+                cy.visit('/login/');
+                cy.get('[type="text"]').type('testandoTeste');
+                cy.get('[type="password"]').type('testandoTeste');
+                cy.get('.btn-login').click();
+            }
+            cy.get('.btn-logout').click();
+        });
+    })
+    it('Should return 401 error when non-authorized login tryes to login', () => {
+        cy.visit('/login/');
+        cy.get('[type="text"]').type('testandoTeste');
+        cy.get('[type="password"]').type('thisisanotherpassword');
+        cy.get('.btn-login')
+            .click()
+            .then((response) => {
+                cy.expect(response.status === 401);
+            });
+    });
+});
