36a36aa107768f24a5a28e1de4ae9d05be27aac7
PedroLimass <phlimas@outlook.com>
2021-05-17 11:33:51 -0300

    Resovendo fetch do questionario e post no contador
    Co-authored-by: Philipe <philipeserafim@gmail.com>

--- frontend/src/components/ShowTree.js
+++ frontend/src/components/ShowTree.js
@@ -1,11 +1,11 @@
 import GraphUI from './GraphUI';
+import { urlGen } from './urls';
+
+const url = urlGen('questionario', 'getQuestionario/.json');
 
 const ShowTree = () => (
     <div>
-        <GraphUI
-            graph_path="/data/graph/graph"
-            question_set_path="/data/questions/sequential"
-        />
+        <GraphUI graph_path="/data/graph/graph" question_set_path={url} />
     </div>
 );
 
